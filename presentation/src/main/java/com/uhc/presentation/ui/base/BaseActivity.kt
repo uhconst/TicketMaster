ddc191805514eaebb0a9293cafb91bcdf14c94a7
Uryel <uryelhenrique.c@gmail.com>
2021-11-13 13:33:48 +0000

    Add base classes

---
+++ presentation/src/main/java/com/uhc/presentation/ui/base/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.uhc.presentation.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseActivity<B : ViewDataBinding> : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    protected val compositeDisposable = CompositeDisposable()
+
+    abstract fun getLayoutRes(): Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, getLayoutRes())
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+}
